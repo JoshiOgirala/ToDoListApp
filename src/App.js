9d74cea27a3a0f4114f0b2f1062f28e440de6c72
JoshiOgirala <168571492+JoshiOgirala@users.noreply.github.com>
2024-05-01 12:37:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
